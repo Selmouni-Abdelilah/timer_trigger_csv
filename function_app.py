2de2c5ac3cddb56197b49f64e5da8a8361e8ede6
Selmouni-Abdelilah <128396251+Selmouni-Abdelilah@users.noreply.github.com>
2024-01-20 20:08:32 +0100

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -1,6 +1,7 @@
 import  datetime
 import  logging
 import  os
+import  tempfile
 import  azure.functions  as  func
 import  csv
 from    azure.storage.fileshare import ShareFileClient, ShareDirectoryClient
@@ -23,10 +24,11 @@ def timer_trigger(mytimer:  func.TimerRequest)  ->  None:
     logging.info('Python timer trigger function ran at %s',  utc_timestamp)
     function_directory = os.getcwd()
     file_name = 'timestamp.csv'
-    file_path = os.path.join(function_directory,  file_name)
+    local_path = tempfile.gettempdir()
+    file_path = os.path.join(local_path, file_name)
     csv_data = [[utc_timestamp]]
     write_csv_file(file_path,  csv_data)
-    conn_string ="DefaultEndpointsProtocol=https;AccountName=timertrigger1234;AccountKey=lUwUhYPr3XqSySO/2fFGUGD6g9qBAWI4dBexO8PRoM5sDN+lTL1KkySKSKADBMdozzeAavJQ7ddi+AStVd9LBg==;EndpointSuffix=core.windows.net"
+    conn_string ="DefaultEndpointsProtocol=https;AccountName=destinationfileshare;AccountKey=vjD7ggihKeLBcnruKEtosfSSOotALfW9YSOg2Bw/QkjpsO69H7Z0WW0fP1YXmNYzOOVj2rv/dcZr+AStNr4vuw==;EndpointSuffix=core.windows.net"
     share_directory_client = ShareDirectoryClient.from_connection_string(conn_str=conn_string, 
                                                             share_name="timerfileshare",
                                                             directory_path="outgoing")
